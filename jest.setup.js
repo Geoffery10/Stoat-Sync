74a6f1677067593e3d35ab6b7b46071d2d28c484
Geoffery Powell <powellgeoffery@gmail.com>
2026-02-18 23:23:24 -0600

    Update jest.setup.js to include coverage configuration

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,8 @@
 import dotenv from 'dotenv';
 dotenv.config({ quiet: true });
+
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Customize as needed
+};
