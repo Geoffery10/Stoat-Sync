accf46b44a4a83c8c44cfd771e70f403458f91cc
Geoffery Powell <powellgeoffery@gmail.com>
2026-02-19 19:11:02 -0600

    Updated jest setup/config

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,1 @@
-import dotenv from 'dotenv';
-dotenv.config({ quiet: true });
-
-module.exports = {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Customize as needed
-};
+require('dotenv').config({ quiet: true });
